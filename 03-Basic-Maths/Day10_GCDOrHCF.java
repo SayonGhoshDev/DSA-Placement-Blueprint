6bed33a55ab92177766df19112a4ffcba88babec
Sayon Ghosh <er.sayonghosh@gmail.com>
2026-02-20 23:43:34 +0530

    feat(math): solve GCD/HCF using Euclidean algorithm

---
+++ 03-Basic-Maths/Day10_GCDOrHCF.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day10_GCDOrHCF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        while (a > 0 && b > 0) {
+            if (a > b) a = a % b;
+            else b = b % a;
+        }
+        if (a == 0) System.out.println(b);
+        else System.out.println(a);
+        sc.close();
+    }
+}
