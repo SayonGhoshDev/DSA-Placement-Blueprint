df05e159d0e3786eb6f36cd47c5c538a89a42b7c
Sayon Ghosh <er.sayonghosh@gmail.com>
2026-03-03 23:06:05 +0530

    feat(array): find maximum and minimum elements

---
+++ 07-Arrays/Day21_MaxMinArray.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Day21_MaxMinArray {
+
+    public static void findMaxMin(int data[]) {
+        if (data.length == 0) {
+            return;
+        }
+
+        int max = data[0];
+        int min = data[0];
+
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] > max) {
+                max = data[i];
+            }
+            if (data[i] < min) {
+                min = data[i];
+            }
+        }
+
+        System.out.println("Maximum element: " + max);
+        System.out.println("Minimum element: " + min);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+        int data[] = new int[n];
+
+        System.out.print("Enter elements: ");
+        for (int i = 0; i < n; i++) {
+            data[i] = sc.nextInt();
+        }
+
+        findMaxMin(data);
+        sc.close();
+    }
+}
